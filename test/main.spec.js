bc5f351a8051483e1a6e9b635b958faca42c1f3b
Elad Bezalel <elad.bezalel@gmail.com>
2015-07-22 17:52:47 +0300

    initial work on testing and update dependencies

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,4 +1,25 @@
 'use strict';
 
+var chai = require('chai');
+var expect = chai.expect;
+
 describe('server-api', function () {
+  describe('exports', function () {
+    var serverApi = require('../lib');
+
+    it('should expose an object', function () {
+      return expect(serverApi).to.be.an.object;
+    });
+
+    it('should have connect function', function () {
+      return expect(serverApi.connect).to.be.a.function;
+    });
+
+    it('should have connected function', function () {
+      return expect(serverApi.connected).to.be.a.function;
+    });
+  });
+
+  describe('connect', function () {
+  });
 });
