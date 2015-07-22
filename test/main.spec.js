3d3d9f497d1bf308ed910a47c5f8ead1ad660be8
Noam Okman <noamokman@gmail.com>
2015-07-22 19:57:08 +0300

    added resource check

--- test/main.spec.js
+++ test/main.spec.js
@@ -8,15 +8,20 @@ describe('server-api', function () {
     var serverApi = require('../lib');
 
     it('should expose an object', function () {
-      return expect(serverApi).to.be.an.object;
+      expect(serverApi).to.be.an.object;
     });
 
     it('should have connect function', function () {
-      return expect(serverApi.connect).to.be.a.function;
+      expect(serverApi.connect).to.be.a.function;
     });
 
     it('should have connected function', function () {
-      return expect(serverApi.connected).to.be.a.function;
+      expect(serverApi.connected).to.be.a.function;
+    });
+
+    it('should have resources', function () {
+      expect(serverApi.packages).to.be.a.function;
+      expect(serverApi.users).to.be.a.function;
     });
   });
 
