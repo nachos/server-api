3cd7c030ebc3dfe7a86e92a61427d3c049d0e6d4
Noam Okman <noamokman@gmail.com>
2015-08-04 20:46:43 +0300

    fixed testing

--- test/main.spec.js
+++ test/main.spec.js
@@ -21,6 +21,7 @@ describe('server-api', function () {
 
       expect(client.connected).to.be.a('function');
       expect(client.connect).to.be.a('function');
+      expect(client.setToken).to.be.a('function');
     });
   });
 
@@ -36,8 +37,12 @@ describe('server-api', function () {
     describe('with invalid data', function () {
       var client = require('../lib')();
 
-      it('should be rejected', function () {
-        expect(client.connect({})).to.eventually.be.rejectedWith(TypeError);
+      it('should reject strings', function () {
+        expect(client.connect('hola')).to.eventually.be.rejectedWith(TypeError);
+      });
+
+      it('should reject numbers', function () {
+        expect(client.connect(5)).to.eventually.be.rejectedWith(TypeError);
       });
     });
 
@@ -74,40 +79,121 @@ describe('server-api', function () {
         return expect(client.connect({email: 'test', password: 'test'})).to.eventually.equal('test');
       });
     });
+  });
 
-    describe('with invalid email', function () {
-      var client;
+  describe('connected', function () {
+    var client;
 
-      before(function () {
-        var apiTailorMock = function () {
-          return {
-            local: {
-              login: sinon.stub().returns(Q.reject())
-            },
-            inject: sinon.stub().returns(Q.resolve())
-          };
+    before(function () {
+      var apiTailorMock = function () {
+        return {
+          local: {
+            login: sinon.stub().returns(Q.resolve({token: 'test'}))
+          },
+          inject: sinon.stub().returns(Q.resolve())
         };
+      };
 
-        mockery.registerMock('api-tailor', apiTailorMock);
+      mockery.registerMock('api-tailor', apiTailorMock);
 
-        mockery.enable({
-          useCleanCache: true,
-          warnOnReplace: false,
-          warnOnUnregistered: false
+      mockery.enable({
+        useCleanCache: true,
+        warnOnReplace: false,
+        warnOnUnregistered: false
+      });
+
+      client = require('../lib')();
+    });
+
+    after(function () {
+      mockery.deregisterMock('api-tailor');
+      mockery.disable();
+    });
+
+    it('should be false before connect', function () {
+      expect(client.connected()).to.be.false;
+    });
+
+    it('should be true after connect', function () {
+      return client.connect({email: 'test', password: 'test'})
+        .then(function () {
+          expect(client.connected()).to.be.true;
         });
+    });
+  });
 
-        client = require('../lib')();
+  describe('setToken', function () {
+    var client = require('../lib')();
+
+    it('should change connected state', function () {
+      expect(client.connected()).to.be.false;
+      client.setToken('test');
+      expect(client.connected()).to.be.true;
+    });
+  });
+
+  describe('token injection', function () {
+    var client;
+    var options;
+
+    before(function () {
+      var apiTailorMock = function () {
+        return {
+          local: {
+            login: sinon.stub().returns(Q.resolve({token: 'test'}))
+          },
+          inject: function (opts) {
+            options = opts;
+          }
+        };
+      };
+
+      mockery.registerMock('api-tailor', apiTailorMock);
+
+      mockery.enable({
+        useCleanCache: true,
+        warnOnReplace: false,
+        warnOnUnregistered: false
+      });
+    });
+
+    beforeEach(function () {
+      client = require('../lib')();
+    });
+
+    after(function () {
+      mockery.deregisterMock('api-tailor');
+      mockery.disable();
+    });
+
+    it('should inject interceptor', function () {
+      expect(options).to.not.be.empty;
+      expect(options.request).to.not.be.empty;
+    });
+
+    describe('without token', function () {
+      it('should set json', function () {
+        var request = {};
+
+        options.request(request);
+        expect(request.json).to.be.true;
+        expect(request.headers).to.be.empty;
+      });
+    });
+
+    describe('with token', function () {
+      beforeEach(function () {
+        client.setToken('test');
       });
 
-      after(function () {
-        mockery.deregisterMock('api-tailor');
-        mockery.disable();
-      });
+      it('should put headers on request', function () {
+        var request = {};
 
-      it('should be resolved with statusCode 401', function () {
-        return expect(client.connect({email: 'test', password: 'test'})).to.be.rejected;
+        options.request(request);
+        expect(request.json).to.be.true;
+        expect(request.headers).to.not.be.empty;
+        expect(request.headers.Authorization).to.equal('Bearer test');
       });
     });
   });
-})
-;
+});
