93038d20ae9cab8242c9234b14c2ae60b87b1fbb
adirshaban <adirshaban1995@gmail.com>
2015-08-04 19:08:12 +0300

    Tests the connect function

--- test/main.spec.js
+++ test/main.spec.js
@@ -2,29 +2,112 @@
 
 var chai = require('chai');
 var expect = chai.expect;
+var Q = require('q');
+var sinon = require('sinon');
+var mockery = require('mockery');
+
+chai.use(require('chai-as-promised'));
 
 describe('server-api', function () {
   describe('exports', function () {
     var serverApi = require('../lib');
 
-    it('should expose an object', function () {
-      expect(serverApi).to.be.an.object;
+    it('should expose a valid exports', function () {
+      expect(serverApi).to.be.a('function');
     });
 
-    it('should have connect function', function () {
-      expect(serverApi.connect).to.be.a.function;
-    });
+    it('client should expose valid exports', function () {
+      var client = serverApi();
 
-    it('should have connected function', function () {
-      expect(serverApi.connected).to.be.a.function;
-    });
-
-    it('should have resources', function () {
-      expect(serverApi.packages).to.be.a.function;
-      expect(serverApi.users).to.be.a.function;
+      expect(client.connected).to.be.a('function');
+      expect(client.connect).to.be.a('function');
     });
   });
 
   describe('connect', function () {
+    describe('without data', function () {
+      var client = require('../lib')();
+
+      it('should be rejected', function () {
+        expect(client.connect()).to.eventually.be.rejectedWith(TypeError);
+      });
+    });
+
+    describe('with invalid data', function () {
+      var client = require('../lib')();
+
+      it('should be rejected', function () {
+        expect(client.connect({})).to.eventually.be.rejectedWith(TypeError);
+      });
+    });
+
+    describe('with valid email and password', function () {
+      var client;
+
+      before(function () {
+        var apiTailorMock = function () {
+          return {
+            local: {
+              login: sinon.stub().returns(Q.resolve({token: 'test'}))
+            },
+            inject: sinon.stub().returns(Q.resolve())
+          };
+        };
+
+        mockery.registerMock('api-tailor', apiTailorMock);
+
+        mockery.enable({
+          useCleanCache: true,
+          warnOnReplace: false,
+          warnOnUnregistered: false
+        });
+
+        client = require('../lib')();
+      });
+
+      after(function () {
+        mockery.deregisterMock('api-tailor');
+        mockery.disable();
+      });
+
+      it('should be resolved with token', function () {
+        return expect(client.connect({email: 'test', password: 'test'})).to.eventually.equal('test');
+      });
+    });
+
+    describe('with invalid email', function () {
+      var client;
+
+      before(function () {
+        var apiTailorMock = function () {
+          return {
+            local: {
+              login: sinon.stub().returns(Q.reject())
+            },
+            inject: sinon.stub().returns(Q.resolve())
+          };
+        };
+
+        mockery.registerMock('api-tailor', apiTailorMock);
+
+        mockery.enable({
+          useCleanCache: true,
+          warnOnReplace: false,
+          warnOnUnregistered: false
+        });
+
+        client = require('../lib')();
+      });
+
+      after(function () {
+        mockery.deregisterMock('api-tailor');
+        mockery.disable();
+      });
+
+      it('should be resolved with statusCode 401', function () {
+        return expect(client.connect({email: 'test', password: 'test'})).to.be.rejected;
+      });
+    });
   });
-});
+})
+;
